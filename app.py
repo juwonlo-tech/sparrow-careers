5217642914887b7e41b756cc8948091233d19077
juwonlo-tech <77176412+juwonlo-tech@users.noreply.github.com>
2023-10-24 19:43:22 +0000

    Made changes

--- app.py
+++ app.py
@@ -2,20 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-JOBS = [
-  {'id': 1, 'title': 'Python Developer', 'location': 'New York', 'salary': 50000
-  },
-  {'id': 2, 'title': 'Data Analyst', 'location': 'Calgary', 'salary': 65000
-  },
-  {'id': 3, 'title': 'Cloud Engineer', 'location': 'New Delhi', 'salary': 108000},
-  {'id': 4, 'title': 'UI/UX Developer', 'location': 'Remote'
-  }
-]
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'New York',
+    'salary': 50000
+}, {
+    'id': 2,
+    'title': 'Data Analyst',
+    'location': 'Calgary',
+    'salary': 65000
+}, {
+    'id': 3,
+    'title': 'Cloud Engineer',
+    'location': 'New Delhi',
+    'salary': 120000
+}, {
+    'id': 4,
+    'title': 'UI/UX Developer',
+    'location': 'Remote'
+}]
+
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html',
-                          jobs=JOBS)
+  return render_template('home.html', jobs=JOBS)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)
