55ac4ed459b317e453882d7084bc208935620a37
juwonlo-tech <77176412+juwonlo-tech@users.noreply.github.com>
2023-11-08 19:55:21 +0000

    additional templates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -15,6 +15,30 @@ def hello_world():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route('/job/<id>')
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  
+  return render_template('jobpage.html', 
+                         job=job)
+  
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html', 
+                         application=data,
+                         job=job)
+
 
 
 if __name__ == '__main__':
